83291e5e28363a2e3fab36b03b168aef969ab9a1
srk-333 <sauravmk333@gmail.com>
2022-01-06 15:38:50 +0530

    [saurav]Added Program to get harmonic number sum

---
+++ Nubersprogram/HarmonicNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nubersprogram
+{
+    internal class HarmonicNumber
+    {
+        public static void PrintHarmonicNumber()
+        {
+            Console.WriteLine("Enter a number to print Harmonic Number");
+            int number=Convert.ToInt32(Console.ReadLine());
+
+            double harmonicSum = 0.0;
+            for (int count = 0; count <= number; count++)
+            {
+                if (count == 0)
+                    Console.WriteLine("please enter positive number");
+                else
+                  harmonicSum += 1 /(float) count;
+            }
+            Console.WriteLine("harmonic Number is: " +harmonicSum);
+        }
+    }
+}
--- Nubersprogram/Program.cs
+++ Nubersprogram/Program.cs
@@ -14,6 +14,7 @@ namespace Nubersprogram
             UserStringChange.userInput();
             FlipCoin.CoinFlip();
             Factors.FactorOfNumber();
+            HarmonicNumber.PrintHarmonicNumber();
             Console.ReadLine();
         }
     }
