7deb89047d918cb193b0c0071914f6495ff3fa3e
srk-333 <sauravmk333@gmail.com>
2022-01-06 12:21:03 +0530

    [saurav] Added Welcome Program

---
+++ Nubersprogram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nubersprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Numbers Related Program");
+            Console.ReadLine();
+        }
+    }
+}
