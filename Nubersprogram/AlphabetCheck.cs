a35646bcacecdb8a8f45656bee7c9c65c670f47f
srk-333 <sauravmk333@gmail.com>
2022-01-15 22:26:50 +0530

    [Saurav]Added Alphabet Check Program

---
+++ Nubersprogram/AlphabetCheck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nubersprogram
+{
+    internal class AlphabetCheck
+    {
+        public static void VowelCheck()
+        {
+            Console.WriteLine("Enter Alphabet to check vowel or consonant");
+            string alph = Console.ReadLine();
+            if (alph == "A"||alph == "a"||alph == "E"||alph == "e"||alph == "I"||alph == "i"||alph == "O"||alph == "o"||alph == "U"||alph == "u")
+            {
+                Console.WriteLine(alph + ": is a vowel");
+            }
+            else
+            {
+                Console.WriteLine(alph + ": is a Consonant");
+            }
+        }
+    }
+}
