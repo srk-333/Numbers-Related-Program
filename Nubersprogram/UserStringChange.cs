cb2b5e81d22f44cfb82fb06735c28766dc635dd0
srk-333 <sauravmk333@gmail.com>
2022-01-17 20:35:38 +0530

    [Saurav]Added comments in program

--- Nubersprogram/UserStringChange.cs
+++ Nubersprogram/UserStringChange.cs
@@ -10,11 +10,13 @@ namespace Nubersprogram
     {
         public static void userInput()
         {
+            //Taking user input as string
             Console.WriteLine("Enter your username");
             String Name = Console.ReadLine();
-
+            //Checking string length
             if (Name.Length >= 3)
             {
+                //calling Method
                 ReplaceGivenTemplate(Name);
             }
             else
@@ -22,13 +24,13 @@ namespace Nubersprogram
                 Console.WriteLine("Please enter atleast 3 characters");
             }
         }
-
+        // In method changing <UserName> from given Template with user input string
         public static void ReplaceGivenTemplate(String Name)
         {
             String Template = "Hello <UserName> , How are you?";
             if (Template.Contains("<UserName>"))
             {
-                string result = Template.Replace("<UserName>", Name);
+                string result = Template.Replace("<UserName>", Name); //Replace method used to replace string with given new string.
                 Console.WriteLine("After replace : " + result);
             }        
         }
