242f97c9a223677dbd75fa61cbe360a8fdcd6e68
srk-333 <sauravmk333@gmail.com>
2022-01-24 22:39:16 +0530

    [Saurav]Add:CouponNumber program

---
+++ Nubersprogram/CouponNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nubersprogram
+{
+    public class CouponNumbers
+    {
+        //static variable.
+        static int N, random, value, count = 0, distinct = 0;
+        //static Construtor.
+        static CouponNumbers()
+        {
+            Console.WriteLine("Enter N distinct Value");
+            N = Convert.ToInt32(Console.ReadLine());
+        }
+        //Method To Get all Distinct Coupon.
+        public int GenerateDistinct()
+        { 
+            bool[] arr = new bool[N];  //bool Array.
+            while (distinct < N)
+            {
+                random = new Random().Next(N);
+                value = random;
+                Console.WriteLine(value + " ");
+                if (!arr[value]) 
+                {
+                    distinct++;
+                    arr[value] = true;
+                    Console.WriteLine(arr[value] + " ");
+                }
+                count++;
+            }
+             return count;
+        }
+    }
+}
--- Nubersprogram/Program.cs
+++ Nubersprogram/Program.cs
@@ -6,32 +6,95 @@ using System.Threading.Tasks;
 
 namespace Nubersprogram
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Numbers Related Program");
 
-            //calling static Method of all the classes created in this project.
-            UserStringChange.userInput();
-            FlipCoin.CoinFlip();
-            Factors.FactorOfNumber();
-            HarmonicNumber.PrintHarmonicNumber();
-            PowerOfTwo.GetPowerOfTwo();
-            SwapTwonumbers.SwapNumbers();
-            LeapYear.FindLeapYear();
-            AlphabetCheck.VowelCheck();
-            EvenOddCheck.OddEvenCheck();
-            LargestNumber.CheckLargestNumber();
-            ComputeQuotientRemainder.QuotienRemainder();
-            FibonacciSeries.FabSeries();
-            ReverseNumber.RevNumber();
-            PerfectNumber.GetPerfectNumber();
-            PrimeNumber.UserInput();
-            Temperature.TemperatureConversion();
-            SquareRoot.Sqrt();
-            MonthlyPayment.Payment();
+            Program program = new Program();
+            program.MethodInvoke();
             Console.ReadLine();
+        }
+        //Method to Get Option to run program.
+        public void MethodInvoke()
+        { 
+            bool CONTINUE = true;
+            //Loop to select Option.
+            while (CONTINUE)
+            {
+                Console.WriteLine("Enter your Choice");
+                Console.WriteLine("1=UserStringChange, 2=FlipCoin, 3=Factors, 4=HarmoincNumber, 5=PowerOfTwo");
+                Console.WriteLine("6=SwapTwoNumbers, 7=LeapYear, 8=AlphabetCheck, 9=EvenOddCheck, 10=LargestNumber");
+                Console.WriteLine("11=FibonacciSeries, 12=ReverseNumber, 13=PerfectNumber, 14=PrimeNumber, 15=SquareRoot");
+                Console.WriteLine("16=MonthlyPayment, 17=TemperatureConversion, 18=QuotientRemainder, 19=CouponNumbrs, 0=Exit");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        UserStringChange.userInput();
+                        break;
+                    case 2:
+                        FlipCoin.CoinFlip();
+                        break;
+                    case 3:
+                        Factors.FactorOfNumber();
+                        break;
+                    case 4:
+                        HarmonicNumber.PrintHarmonicNumber();
+                        break;
+                    case 5:
+                        PowerOfTwo.GetPowerOfTwo();
+                        break;
+                    case 6:
+                        SwapTwonumbers.SwapNumbers();
+                        break;
+                    case 7:
+                        LeapYear.FindLeapYear();
+                        break;
+                    case 8:
+                        AlphabetCheck.VowelCheck();
+                        break;
+                    case 9:
+                        EvenOddCheck.OddEvenCheck();
+                        break;
+                    case 10:
+                        LargestNumber.CheckLargestNumber();
+                        break;
+                    case 11:
+                        FibonacciSeries.FabSeries();
+                        break;
+                    case 12:
+                        ReverseNumber.RevNumber();
+                        break;
+                    case 13:
+                        PerfectNumber.GetPerfectNumber();
+                        break;
+                    case 14:
+                        PrimeNumber.UserInput();
+                        break;
+                    case 15:
+                        SquareRoot.Sqrt();
+                        break;
+                    case 16:
+                        MonthlyPayment.Payment();
+                        break;
+                    case 17:
+                        Temperature.TemperatureConversion();
+                        break;
+                    case 18:
+                        ComputeQuotientRemainder.QuotienRemainder();
+                        break;
+                    case 19:
+                        CouponNumbers couponNumbers = new CouponNumbers();
+                        int count = couponNumbers.GenerateDistinct();
+                        Console.WriteLine("NO of Distinct Coupons Generated: "+count);                      
+                        break;
+                    default:
+                        CONTINUE = false;
+                        break;
+                }
+            }
         }
     }
 }
